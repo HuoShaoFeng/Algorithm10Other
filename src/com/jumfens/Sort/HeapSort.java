d0dae45c9cbe53991d28ea861154eb585e815247
HuoShaoFeng <huo_shao_feng@163.com>
2018-03-12 19:56:18 +0800

    堆排序实现

--- src/com/jumfens/Sort/HeapSort.java
+++ src/com/jumfens/Sort/HeapSort.java
@@ -3,7 +3,7 @@ package com.jumfens.Sort;
 public class HeapSort {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1,6,-2,8,3,3,9,7,2};
+        int[] arr = new int[]{31,6,-2,8,33,3,-9,7,2};
         heapSort(arr);
 
         for (int i = 0; i < arr.length; i++) {
@@ -13,28 +13,32 @@ public class HeapSort {
 
     private static void heapSort(int[] arr) {
 
-        //构建一个大顶堆
+        //构建一个小顶堆
         for (int i=arr.length/2-1;i>=0;--i){
             heapAdjust(arr,i,arr.length-1);
         }
 
         for (int i = arr.length-1; i >=0 ; i--) {
-            int tmp = arr[0];
+            //交换堆顶记录 和 当前未经排序子数组的 最后一个元素
+            int tmp = arr[0];//arr[0]是堆顶元素
             arr[0]=arr[i];
             arr[i]=tmp;
-            heapAdjust(arr,0,i-1);
+            heapAdjust(arr,0,i-1);//i-1就是未经排序的最后一个元素
         }
     }
 
+    //调整堆为一个小顶堆
     private static void heapAdjust(int[] arr, int start, int stop) {
         int tmp = arr[start];
         for (int i = start*2+1; i <=stop; i=i*2+1) {
-            if(i<stop && arr[i]>arr[i+1])//指向较小的子节点
-                i++;
-            if(arr[i]>tmp)
+            if(i<stop && arr[i]>arr[i+1])
+                i++;//指向较小的子节点
+
+            if(arr[i]>tmp)//较小的子节点 大于 父节点，无需调整
                 break;
-            arr[start]=arr[i];
-            start=i;
+
+            arr[start]=arr[i];//将较小的子节点 赋值 给其父节点
+            start=i;//继续对start的较小的子节点进行堆调整
         }
         arr[start]=tmp;
     }
